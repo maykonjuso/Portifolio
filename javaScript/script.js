255222de1973eaf792fcb5e0f3617207f1755134
Maykon <maykola1331@gmail.com>
2021-10-22 15:43:13 -0300

    VALEU

---
+++ javaScript/script.js
@@ -0,0 +1,47 @@
+var menuItens1 = document.querySelectorAll('.seta');
+
+menuItens1.forEach(item => {
+    item.addEventListener('click', eventoClick);
+})
+
+var menuItens2 = document.querySelectorAll('.home1');
+
+menuItens1.forEach(item => {
+    item.addEventListener('click', eventoClick);
+})
+
+menuItens2.forEach(item => {
+    item.addEventListener('click', eventoClick);
+})
+
+function eventoClick(event) {
+    event.preventDefault();
+    var elemento = event.target;
+    var id = elemento.getAttribute('href');
+    var to = document.querySelector(id).offsetTop;
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    });
+}
+
+// animação scroll
+
+var target = document.querySelectorAll('[data-animacao]');
+var classeAnimacao = 'animate';
+
+function animacaoScroll() {
+    var windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if((windowTop) > element.offsetTop){
+            element.classList.add(classeAnimacao);
+        } else{
+            element.classList.remove(classeAnimacao);
+        }
+
+    });
+}
+
+window.addEventListener('scroll', function() {
+    animacaoScroll();
+}) 
